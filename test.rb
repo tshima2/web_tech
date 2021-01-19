97894ca6dde2f6030a21d449d4d05f72c984b064
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-01-19 23:11:56 +0900

    htmlpage entry (DocumentRoot / URL) add in test.rb

--- test.rb
+++ test.rb
@@ -7,6 +7,8 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
+
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'index.html.erb')
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
